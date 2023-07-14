d183520af1b0a87a342be5daecadebfd698370d7
BayuDC <dev.bayudc@gmail.com>
2023-07-15 06:17:59 +0700

    fix: remove slow google fonts module

--- app.vue
+++ app.vue
@@ -7,6 +7,13 @@ const appConfig = useAppConfig();
         <Head>
             <Title>{{ appConfig.name }} | {{ appConfig.description }}</Title>
             <Meta name="description" :content="appConfig.description" />
+
+            <Link rel="preconnect" href="https://fonts.googleapis.com" />
+            <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+            <Link
+                rel="stylesheet"
+                href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;800&display=swap"
+            />
         </Head>
         <Body class="bg-dark text-light">
             <SiteInfo />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
-    googleFonts: {
-        families: {
-            Inter: [700, 800],
-        },
-        preconnect: true,
-    },
+    modules: ['@nuxtjs/tailwindcss'],
 });
