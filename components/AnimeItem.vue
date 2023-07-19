cfcbfd56de283165c095d021804df00dc1886aa6
BayuDC <dev.bayudc@gmail.com>
2023-07-19 16:51:07 +0700

    feat: show airing time and episode

--- components/AnimeItem.vue
+++ components/AnimeItem.vue
@@ -26,12 +26,25 @@ useMotion(target, {
         },
     },
 });
+
+const airing = computed<string>(() => {
+    const date = new Date(0);
+    date.setUTCSeconds(props.anime.nextAiringEpisode.airingAt);
+    return date.toLocaleString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true });
+});
 </script>
 
 <template>
     <li>
         <div ref="target">
-            <div class="w-full rounded-lg overflow-hidden aspect-[5/7] bg-dark2">
+            <div class="w-full rounded-lg overflow-hidden aspect-[5/7] bg-dark2 relative">
+                <div class="absolute top-0 left-0 right-0 z-10 flex justify-between p-2">
+                    <AnimeItemBadge class="bg-secondary">
+                        {{ airing }}
+                    </AnimeItemBadge>
+                    <AnimeItemBadge class="bg-primary">Ep {{ anime.nextAiringEpisode.episode }}</AnimeItemBadge>
+                </div>
+
                 <img
                     class="object-cover w-full h-full transition duration-300"
                     :class="[imgLoaded ? 'opacity-100 blur-none' : 'opacity-0 blur']"
---
+++ components/AnimeItemBadge.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+    <div class="text-xs font-semibold py-1.5 px-2 rounded-md shadow-sm shadow-dark2">
+        <slot />
+    </div>
+</template>
+
+<style scoped></style>
